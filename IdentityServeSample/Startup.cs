35d596cbddf8246ecd4812d0a37e9f985186f124
IanChen <ian.chen@taiwantaxi.com.tw>
2020-08-06 16:31:58 +0800

    add identity user.

--- IdentityServeSample/Startup.cs
+++ IdentityServeSample/Startup.cs
@@ -1,7 +1,8 @@
 using System.Reflection;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,36 +18,6 @@ namespace IdentityServeSample
         }
 
         public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-
-            var connectionString = Configuration.GetConnectionString("Identity");
-            var assemblyName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-            services.AddIdentityServer()
-                    .AddConfigurationStore(options =>
-                    {
-                        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql =>
-                        {
-                            sql.MigrationsAssembly(assemblyName);
-                        });
-                    })
-                    .AddOperationalStore(options =>
-                    {
-                        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql =>
-                        {
-                            sql.MigrationsAssembly(assemblyName);
-                        });
-                    })
-                    .AddDeveloperSigningCredential();
-
-            // .AddInMemoryApiResources(IdentityConfig.GetApiResources())
-            // .AddInMemoryApiScopes(IdentityConfig.GetScopes())
-            // .AddInMemoryClients(IdentityConfig.GetClients())
-            // .AddInMemoryIdentityResources(IdentityConfig.GetIdentityResources())
-            // .AddDeveloperSigningCredential();
-        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -68,6 +39,41 @@ namespace IdentityServeSample
             {
                 endpoints.MapControllers();
             });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var connectionString = Configuration.GetConnectionString("Identity");
+            var assemblyName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ApplicationDbContext>(builder =>
+            {
+                builder.UseSqlServer(Configuration.GetConnectionString("ApplicationUser"));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddIdentityServer()
+                    .AddAspNetIdentity<ApplicationUser>()
+                    .AddConfigurationStore(options =>
+                    {
+                        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql =>
+                        {
+                            sql.MigrationsAssembly(assemblyName);
+                        });
+                    })
+                    .AddOperationalStore(options =>
+                    {
+                        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql =>
+                        {
+                            sql.MigrationsAssembly(assemblyName);
+                        });
+                    })
+                    .AddDeveloperSigningCredential();
         }
     }
 }
