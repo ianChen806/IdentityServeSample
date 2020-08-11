8e16e506e00578b2270e31652ac4b170f49dcfb4
IanChen <ian.chen@taiwantaxi.com.tw>
2020-08-11 11:24:14 +0800

    update api side.

--- IdentityServeSample.Api/Controllers/HomeController.cs
+++ IdentityServeSample.Api/Controllers/HomeController.cs
@@ -4,12 +4,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServeSample.Api.Controllers
 {
-    [Authorize]
     [Route("Home/{action}")]
     public class HomeController : ControllerBase
     {
         [HttpGet]
+        [Authorize]
         public JsonResult Test()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public JsonResult TestAdmin()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "user")]
+        public JsonResult TestUser()
         {
             return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
         }
--- IdentityServeSample.Api/Startup.cs
+++ IdentityServeSample.Api/Startup.cs
@@ -1,19 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
 
 namespace IdentityServeSample.Api
 {
